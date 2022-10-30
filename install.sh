15f2e75f673b98e07b43eaf0f3c86911580acd2d
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-10-30 13:35:44 +0100

    script

---
+++ install.sh
@@ -0,0 +1,22 @@
+tempFolder="/usr/local/pets-cli"
+appUrl="https://github.com/arielsrv/pets-cli"
+
+if [ -d "$tempFolder" ]; then
+  rm -rf "$tempFolder"
+else
+  mkdir -p "$tempFolder"
+fi
+
+git clone "$appUrl" "$tempFolder"
+python3 -m pip install --upgrade pip setuptools wheel
+pip install -U pyinstaller
+(
+  cd "$tempFolder" || exit
+  pyinstaller pets.py
+)
+
+echo "$tempFolder/dist/pets/pets"
+FILE="/usr/local/bin/pets"
+if [ ! -L "$FILE" ]; then
+  sudo ln -s "$tempFolder/dist/pets/pets" /usr/local/bin/pets
+fi
