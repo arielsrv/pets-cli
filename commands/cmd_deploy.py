12feb8b7d60e220e7c46b130105ac90b8689a9bb
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-25 09:19:21 +0100

    fix

--- commands/cmd_deploy.py
+++ commands/cmd_deploy.py
@@ -1,14 +1,14 @@
 import click
 
 from clients import PetApiClient
-from common.pets_file import get_app_name
-from pets import pass_environment
 
 petApiClient = PetApiClient('http://localhost:8080', 'https://gitlab.tiendanimal.com:8088/')
 
 
-@click.command("describe-infra")
-@pass_environment
-def cli(ctx):
+@click.command("deploy")
+@click.option('-s', '--scope')
+@click.option('-v', '--version')
+@click.option('-str', '--strategy', type=click.Choice(['BLUE-GREEN', 'CANARY', 'ALL-IN'], case_sensitive=False))
+def cli(scope, version, strategy):
     click.echo('scopes...')
     click.echo('secrets...')
