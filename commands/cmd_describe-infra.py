86f7c10f962894ac4c5d708073f73fbc85ba67a8
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-27 10:36:55 +0100

    fix

--- commands/cmd_describe-infra.py
+++ commands/cmd_describe-infra.py
@@ -1,7 +1,6 @@
 import click
 
 from clients import PetApiClient
-from common.pets_file import get_app_name
 from pets import pass_environment
 
 petApiClient = PetApiClient('http://localhost:8080', 'https://gitlab.tiendanimal.com:8088/')
