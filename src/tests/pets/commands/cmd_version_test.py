e016da4fa969808723fbc711f39ac393f300ef22
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-27 18:18:40 +0100

    fix

--- src/tests/pets/commands/cmd_version_test.py
+++ src/tests/pets/commands/cmd_version_test.py
@@ -1,8 +1,17 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from click.testing import CliRunner
+
+from src.pets.commands.cmd_version import cli
+
+
+class CmdVersionTest(unittest.TestCase):
+    def test_get_version(self):
+        runner = CliRunner()
+        actual = runner.invoke(cli)
+        self.assertEqual(0, actual.exit_code)
+        self.assertEqual('0.0.8\n', actual.output)
+
 
 if __name__ == '__main__':
     unittest.main()
