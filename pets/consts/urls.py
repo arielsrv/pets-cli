242889c46c73c240c304454ae410c0552aa2e85d
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-28 15:41:38 +0100

    fix

--- pets/consts/urls.py
+++ pets/consts/urls.py
@@ -1,3 +1,3 @@
-PETS_FILE_NAME = ".pets"
-PETS_API_URL = "http://localhost:8080"
-GITLAB_API_URL = "https://gitlab.tiendanimal.com:8088/"
+PETS_FILE_NAME = '.pets'
+PETS_API_URL = 'http://localhost:8080'
+GITLAB_API_URL = 'https://gitlab.tiendanimal.com:8088/'
