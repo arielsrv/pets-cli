c4cf8c5a52d15c3338f4541aec79d56bf533b223
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-28 14:43:23 +0100

    fix

--- pets/commands/cmd_get.py
+++ pets/commands/cmd_get.py
@@ -3,10 +3,9 @@ from subprocess import call
 import click
 
 from pets.clients.petsapiclient import PetApiClient
-from pets.consts.urls import PETS_API_URL, GITLAB_API_URL
 from pets.pets import pass_environment
 
-petApiClient = PetApiClient(PETS_API_URL, GITLAB_API_URL)
+petApiClient = PetApiClient("PETS_API_URL", "GITLAB_API_URL")
 
 
 @click.command("get")
