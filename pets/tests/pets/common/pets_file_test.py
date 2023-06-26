36fa63d2a194f6bc3019ca7da7f1cfa89169942b
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-06-26 09:02:56 +0200

    update

--- pets/tests/pets/common/pets_file_test.py
+++ pets/tests/pets/common/pets_file_test.py
@@ -5,12 +5,12 @@ from pets.common.pets_file import get_app_name
 
 class PetsFileTest(unittest.TestCase):
     def test_get_app_name(self):
-        appName = get_app_name(".pets")
-        self.assertEqual('pets-cli', appName)
+        appname = get_app_name(".pets")
+        self.assertEqual('pets-cli', appname)
 
     def test_get_app_name_not_found(self):
-        appName = get_app_name(".not_found")
-        self.assertIsNone(appName)
+        appname = get_app_name(".not_found")
+        self.assertIsNone(appname)
 
 
 if __name__ == '__main__':
